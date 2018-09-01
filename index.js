da8ba3f67412817291887bc5468f057212baa3bc
Abbey Mondshein <abbey.sync@gmail.com>
2018-09-01 22:52:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function takeANumber(line,name){
+  line.push(name);
+  var lineNumber = line.length;
+  return `Welcome, ${name}. You are number ${lineNumber} in line.`
+}
+
+function nowServing(katzDeliLine){
+  let i=0
+  while (i<katzDeliLine.length){
+  console.log( `Currently serving ${katzDeliLine[0]}.`);
+  katzDeliLine.shift();
+}
+while (katzDeliLine.length===0){
+  return "There is nobody waiting to be served!"
+}
+  
+}
