1bcf07fae6769619753c1f75e4399165a2be6101
Abbey Mondshein <abbey.sync@gmail.com>
2018-09-02 00:03:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ if (katzDeliLine.length===0){
  return "There is nobody waiting to be served!"
 }
 else {
-	return  `Currently serving ${katzDeliLine[0].shift()}.`;
+	return  `Currently serving ${katzDeliLine.shift()}.`;
 }
 }
