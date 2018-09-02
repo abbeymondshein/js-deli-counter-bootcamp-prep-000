cfadec3a3c03760b967319a386d32b06105b499f
Abbey Mondshein <abbey.sync@gmail.com>
2018-09-02 00:03:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ if (katzDeliLine.length===0){
  return "There is nobody waiting to be served!"
 }
 else {
-	return  `Currently serving ${katzDeliLine[0]}.`;
+	return  `Currently serving ${katzDeliLine[0].shift()}.`;
 }
 }
