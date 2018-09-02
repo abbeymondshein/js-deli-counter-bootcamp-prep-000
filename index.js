781e121a1cc5a6c8a15c99750b937524c130dd07
Abbey Mondshein <abbey.sync@gmail.com>
2018-09-02 00:09:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function takeANumber(line,name){
-  line.push(name);
-  var lineNumber = line.length;
-  return `Welcome, ${name}. You are number ${lineNumber} in line.`
+	line.push(name);
+	var lineNumber = line.length;
+	return `Welcome, ${name}. You are number ${lineNumber} in line.`
 }
 
 //accepts current line of people, returns first person in line, removes that person from line
 //if nobody is in line, state as such
 function nowServing(katzDeliLine){
-  let i=0
-  while (i<katzDeliLine.length){
-    i++;
-}
+	let i=0
+	while (i<katzDeliLine.length){
+		i++;
+	}
 
-if (katzDeliLine.length===0){
- return "There is nobody waiting to be served!"
-}
-else {
-	return  `Currently serving ${katzDeliLine.shift()}.`;
-}
+	if (katzDeliLine.length===0){
+		return "There is nobody waiting to be served!"
+	}
+	else {
+		return  `Currently serving ${katzDeliLine.shift()}.`;
+	}
 }
